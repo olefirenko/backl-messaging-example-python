349343ab45cb40f8e6cc2ea784a73d0fdae25083
Dmytro Olefyrenko <dmitriy.olefirenko@gmail.com>
2023-08-16 16:10:54 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -14,7 +14,7 @@ def message():
     from pprint import pprint
     p = request.get_json()
     pprint(p)
-    subject = 'Links on your site "{prospect_title}"'.format(**p)
+    subject = 'Enhance Your "{prospect_title}" Site with {my_domain}'.format(**p)
     email = p["email_address"]
     if '.' in email.split('@')[0]:
         name = email.split('@')[0].split('.')[0].capitalize()
@@ -27,13 +27,21 @@ def message():
         extra_section = ''
     body = """Hi {name},
 
-My name is Dmytro and I'm the founder of {my_domain}: Create amazing content 10x faster with AI tools.
+I hope this message finds you well.
 
-I found your site "{prospect_title}" and noticed that you link to similar tools.
+I'm Dmytro, the founder of {my_domain}, a platform dedicated to helping content creators craft compelling content 10 times faster with our sophisticated AI tools.
 
-Question: would you mind adding a link to {my_domain}{extra_section}? It would mean the world to me!
+In my research, I came across your impressive site "{prospect_title}" and noticed that you often link out to tools that empower people in the same way we do.
 
-Thanks a lot,
+I wanted to bring your attention to {my_domain} and consider its potential value for your audience. Would you mind featuring a link to our site{extra_section}?
+
+Moreover, to add icing on the cake, we have an affiliate program that generously rewards our partners with a 30% commission from all referred users' payments! You can get the nitty-gritty of this program here https://easy-peasy-ai.getrewardful.com/signup.
+
+Thank you for considering this opportunity, {name}. I genuinely believe that {my_domain} can be a useful resource for your audience and a profitable addition for you.
+
+Looking forward to hearing from you soon.
+
+Best regards,
 Dmytro
 """.format(name=name, extra_section=extra_section, **p)
     return dict(subject=subject, body=body)
